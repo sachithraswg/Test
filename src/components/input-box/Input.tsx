4c11dd2e20fcace7ccc5da60b5c7bcf672068f7d
SachithraGeeganage <sachithra@entrusttitle.com>
2023-12-17 12:45:14 +0530

    disable button issue fixed

--- src/components/input-box/Input.tsx
+++ src/components/input-box/Input.tsx
@@ -23,44 +23,43 @@ const Input: React.FC<InputProps> = ({ nodes, focusedId, onFocus, onAddNode, onD
   }
 
   return (
-    <>
-      <li
-        className={
-          nodes && nodes.child && nodes.child.length > 0 ? "container" : ""
-        }
-      >
-        <div className="input-wrapper">
-          <span>
-            <input
-              className="input"
-              onClick={() => onFocus(nodes.id)}
-              readOnly={state.isDisabled}
-            />
-            {state.isDisabled && <i className="fa-solid fa-lock disable-icon"></i>}
-          </span>
-          {focusedId === nodes.id && (
-            <NodeController
-              onAddNode={() => onAddNode(nodes.id)}
-              onDisable={handleDisable}
-              onDelete={() => onDeleteNode(nodes.id)}
-              key={nodes.id}
-            ></NodeController>
-          )}
-        </div>
-        <ul>
-          {nodes.child.map((child) => (
-            <Input
-              focusedId={focusedId}
-              nodes={child}
-              onFocus={onFocus}
-              onAddNode={onAddNode}
-              onDeleteNode={onDeleteNode}
-              key={child.id}
-            />
-          ))}
-        </ul>
-      </li>
-    </>
+    <li
+      className={
+        nodes && nodes.child && nodes.child.length > 0 ? "container" : ""
+      }
+    >
+      <div className="input-wrapper">
+        <span>
+          <input
+            className="input"
+            onClick={() => onFocus(nodes.id)}
+            readOnly={state.isDisabled}
+          />
+          {state.isDisabled && <i className="fa-solid fa-lock disable-icon"></i>}
+        </span>
+        {focusedId === nodes.id && (
+          <NodeController
+            onAddNode={() => onAddNode(nodes.id)}
+            onDisable={handleDisable}
+            onDelete={() => onDeleteNode(nodes.id)}
+            disabled={state.isDisabled}
+            key={nodes.id}
+          ></NodeController>
+        )}
+      </div>
+      <ul>
+        {nodes.child.map((child) => (
+          <Input
+            focusedId={focusedId}
+            nodes={child}
+            onFocus={onFocus}
+            onAddNode={onAddNode}
+            onDeleteNode={onDeleteNode}
+            key={child.id}
+          />
+        ))}
+      </ul>
+    </li>
   );
 }
 
--- src/components/node-controller/NodeController.tsx
+++ src/components/node-controller/NodeController.tsx
@@ -4,32 +4,31 @@ import "./NodeController.css";
 interface NodeControllerProps {
   onAddNode: () => void,
   onDisable: (isDisabled: boolean) => void,
-  onDelete: () => void
+  onDelete: () => void,
+  disabled: boolean
 }
 
-const NodeController: React.FC<NodeControllerProps> = ({ onAddNode, onDisable, onDelete }) => {
+const NodeController: React.FC<NodeControllerProps> = ({ onAddNode, onDisable, onDelete, disabled }) => {
   function handleDisable(event: React.ChangeEvent<HTMLInputElement>) {
     onDisable(event.target.checked);
   }
 
   return (
-    <>
-      <div className="action-container">
-        <div className="action-panel">
-          <label className="label">Read only</label>
-          <label className="switch">
-            <input type="checkbox" onChange={handleDisable} />
-            <span className="slider round"></span>
-          </label>
-          <button className="delete-button" onClick={onDelete}>
-            <i className="fa fa-trash" aria-hidden="true"></i>
-          </button>
-        </div>
-        <button className="add-btn" onClick={onAddNode}>
-          +
+    <div className="action-container">
+      <div className="action-panel">
+        <label className="label">Read only</label>
+        <label className="switch">
+          <input type="checkbox" checked={disabled} onChange={handleDisable} />
+          <span className="slider round"></span>
+        </label>
+        <button className="delete-button" onClick={onDelete}>
+          <i className="fa fa-trash" aria-hidden="true"></i>
         </button>
       </div>
-    </>
+      <button className="add-btn" onClick={onAddNode}>
+        +
+      </button>
+    </div>
   );
 }
 
